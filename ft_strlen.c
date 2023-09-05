23b2ef0034157b720703bf1f5a3235eda0f0c452
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-09-05 14:49:17 +0200

    strlen should work now

---
+++ ft_strlen.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fseles <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 14:00:33 by fseles            #+#    #+#             */
+/*   Updated: 2023/09/05 14:00:38 by fseles           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+
+#include <string.h>
+#include <stdlib.h>
+
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0; 
+	while (s[i] != '\0')
+	{
+		i++; 
+	}
+	return (i); 
+}
+
+int main ()
+{
+	if(ft_strlen("Filip") == strlen("Filip"))
+	{
+		printf("isti su \n"); 
+	}
+	else 
+	{
+		printf("nisu \n"); 
+	}
+
+	if(ft_strlen("") == strlen(""))
+	{
+		printf("isti su \n"); 
+	}
+	else 
+	{
+		printf("nisu \n"); 
+	}
+
+
+}
