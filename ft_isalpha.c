a8c184ebdd5a814a2a5f2e941923de70b4b699dc
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-09-05 10:44:19 +0200

    2 ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,14 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fseles <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 10:04:30 by fseles            #+#    #+#             */
+/*   Updated: 2023/09/05 10:05:43 by fseles           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <ctype.h>
 #include <stdio.h>
 
 
-static int ft_isupper(int c)
+static int	ft_isupper(int c)
 {
-    if(c >= 'A' && c <= 'Z')
-    {
-        return (1);
-    }
-    return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+static int	ft_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int ft_isalpha(int c)
+{
+	return (ft_isupper(c) || ft_islower(c));
 }
 
 int main ()
@@ -16,6 +42,27 @@ int main ()
     //int isalpha(int c); // ekvivaletno isupper || islower
     //int isupper(int c);
     printf("%d", isalpha('3')); 
+
+    printf("Original function value is %d\n", islower('a'));
+    printf("Mycopyyy function value is %d\n\n", ft_islower('a'));
+
+    printf("Original function value is %d\n", islower('b'));
+    printf("Mycopyyy function value is %d\n\n", ft_islower('b'));
+
+    printf("Original function value is %d\n", islower('z'));
+    printf("Mycopyyy function value is %d\n\n", ft_islower('z'));
+
+    printf("Original function value is %d\n", islower('a'));
+    printf("Mycopyyy function value is %d\n\n", ft_islower('a'));
+
+    printf("Original function value is %d\n", islower(0));
+    printf("Mycopyyy function value is %d\n\n", ft_islower(0));
+
+    printf("Original function value is %d\n", islower('A'));
+    printf("Mycopyyy function value is %d\n\n", ft_islower('A'));
+
+    printf("Original function value is %d\n", islower('\n'));
+    printf("Mycopyyy function value is %d\n\n", ft_islower('\n'));
 
     return (0);
 }
