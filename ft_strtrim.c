69bca7b540f413fa36f503ca46d9495e481393aa
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-09-08 11:26:41 +0200

    ftstrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,55 +12,55 @@
 
 #include "libft.h"
 
-static int is_in_char_set(char const c, char const *set)
+static int	is_in_char_set(char const c, char const *set)
 {
-	int	i; 
+	int	i;
 
-	i = 0; 
+	i = 0;
 	while (set[i] != '\0')
 	{
-		if(set[i] == c)
-			return (1); 
-		i ++; 
+		if (set[i] == c)
+			return (1);
+		i++;
 	}
-	return (0); 
+	return (0);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t starting_index; 
-	size_t substract_end;
-	size_t i;
-	char *p;
+	size_t	start_index;
+	size_t	sub_end;
+	size_t	i;
+	char	*p;
 
-	starting_index = 0; 
-	substract_end = 0;
+	start_index = 0;
+	sub_end = 0;
 	i = 0;
-	while(is_in_char_set(s1[starting_index], set))
-		starting_index ++; 
-	while(is_in_char_set(s1[ft_strlen(s1) - 1 - substract_end], set))
-		substract_end ++; 
-	p = ft_calloc(ft_strlen(s1) + 1 - starting_index - substract_end, sizeof(char));
-	if(p == NULL)
-		return(NULL); 
-	while(starting_index < (ft_strlen(s1) - substract_end))
+	while (is_in_char_set (s1[start_index], set))
+		start_index ++;
+	while (is_in_char_set (s1[ft_strlen(s1) - 1 - sub_end], set))
+		sub_end ++;
+	p = ft_calloc (ft_strlen(s1) + 1 - start_index - sub_end, sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	while (start_index < (ft_strlen(s1) - sub_end))
 	{
-		p[i] = s1[starting_index]; 
+		p[i] = s1[start_index];
 		i++;
-		starting_index++;
+		start_index++;
 	}
 	return (p);
 }
 
-#include<stdio.h>
-int main()
-{
-	char s[] = " Filip  ";
-	char s1[] = " ";
-	char *p1; 
+// #include<stdio.h>
+// int main()
+// {
+// 	char s[] = " Filip  ";
+// 	char s1[] = " ";
+// 	char *p1; 
 
-	p1 = ft_strtrim(s, s1); 
-	printf("%s", p1); 
-	free(p1); 
-	return (0); 
-}
+// 	p1 = ft_strtrim(s, s1); 
+// 	printf("%s", p1); 
+// 	free(p1); 
+// 	return (0); 
+// }
