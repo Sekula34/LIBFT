72771ff84578074abf36242b2e0a3f87aa0284b6
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-09-09 16:48:50 +0200

    mandatory

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -12,16 +12,16 @@
 
 #include "libft.h"
 
-void ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
 }
 
-int main()
-{
-	char s[] = "Jel ovo radi pliz reci da da ";
-	ft_putendl_fd(s, 1);
-	ft_putendl_fd(s, 1);
-	return (0);
-}
+// int main()
+// {
+// 	char s[] = "Jel ovo radi pliz reci da da ";
+// 	ft_putendl_fd(s, 1);
+// 	ft_putendl_fd(s, 1);
+// 	return (0);
+// }
