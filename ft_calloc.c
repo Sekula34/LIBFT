a4fe6b05f579737410ee616e4ebdfba02e3924d1
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-09-11 17:32:09 +0200

    i have some bulluneseseri code in

--- ft_calloc.c
+++ ft_calloc.c
@@ -15,13 +15,11 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*p;
-
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+	
 	p = malloc (size * nmemb);
 	if (p == NULL)
 		return (NULL);
-	ft_bzero (p, nmemb);
+	ft_bzero (p, nmemb * size);
 	return (p);
 }
 
