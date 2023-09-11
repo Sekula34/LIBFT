463d95f1be53514443e9e95981fa34a9af9b0c58
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-09-11 15:14:18 +0200

    bugs

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,13 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*sa;
+	unsigned char	*sb;
 
+	sa = (unsigned char *) s1;
+	sb = (unsigned char *) s2;
 	i = 0;
 	while ((n - i) > 0 && (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (sa[i] != sb[i])
+			return ((int)(sa[i] - sb[i]));
 		i++;
 	}
 	return (0);
@@ -33,17 +37,17 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 //    char str2[20];
 //    int result;
 //    //Assigning the value to the string str1
-//    strcpy(str1, "Filip");
+//    strcpy(str1, "test\200");
 //    //Assigning the value to the string str2
-//    strcpy(str2, "Filip Seles");
+//    strcpy(str2, "test\0");
 //    //This will compare the first 3 characters
 //    result = ft_strncmp(str1, str2, 5);
 //    if(result > 0) {
 //       printf("ASCII value of 
 //first unmatched character of str1 is greater than str2");
 //    } else if(result < 0) {
-//       printf("ASCII value of first unmatched 
-//character of str1 is less than str2");
+//       printf("ASCII value of first 
+//unmatched character of str1 is less than str2");
 //    } else {
 //       printf("Both the strings str1 and str2 are equal");
 //    }
