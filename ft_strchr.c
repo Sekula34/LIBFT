8699275b55a6f28628e918f48eb8ee369893bdd8
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-09-12 10:24:51 +0200

    again bugs

--- ft_strchr.c
+++ ft_strchr.c
@@ -19,21 +19,21 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		if (s[i] == (char) c)
 			return ((char *)s + i);
 		i++;
 	}
-	if (c == '\0')
+	if ((char) c == '\0')
 		return ((char *) s + i);
 	return (NULL);
 }
-// #include <stdio.h>
-// #include <string.h>
-// int main () {
-//    const char str[] = "teste"; 
-//    const char ch = 'e'; 
-//    char *p;
-//    p = ft_strchr(str, ch);
-//    printf("String starting from %c is: %s", ch, p);
-//    return 0;
-// }
+#include <stdio.h>
+#include <string.h>
+int main () {
+   const char str[] = "teste"; 
+   const char ch = '\0'; 
+   char *p;
+   p = ft_strchr(str, ch);
+   printf("String starting from %c is: %s", ch, p);
+   return 0;
+}
