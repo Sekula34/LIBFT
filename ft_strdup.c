bf531aa53a663e98671d944f073c2b1a51f8a242
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-09-13 13:09:36 +0200

    now

--- ft_strdup.c
+++ ft_strdup.c
@@ -24,18 +24,18 @@ char	*ft_strdup(const char *s)
 	return (p);
 }
 
-#include<stdio.h>
-#include<string.h>
+// #include<stdio.h>
+// #include<string.h>
 
-int main()
-{
-    char source[] = "0123456789";
+// int main()
+// {
+//     char source[] = "0123456789";
 
-    // A copy of source is created dynamically
-    // and pointer to copy is returned.
-    char* target = ft_strdup(source);
+//     // A copy of source is created dynamically
+//     // and pointer to copy is returned.
+//     char* target = ft_strdup(source);
 
-    printf("%s", target);
-	free(target);
-    return 0;
-}
+//     printf("%s", target);
+// 	free(target);
+//     return 0;
+// }
